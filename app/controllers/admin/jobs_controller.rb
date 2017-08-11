8bbaf0e1a255b8e53b89a4c3a90e0d818951b129
like <20783506632@qq.com>
2017-08-11 09:28:22 +0800

    implement admin CURD

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
-class JobsController < ApplicationController
+class Admin::JobsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+	before_action :required_admin
     def index
     	@jobs = Job.all
     end
@@ -8,18 +9,18 @@ class JobsController < ApplicationController
     	@job = Job.find(params[:id])
     end
 
+    def show
+    	@job = Job.find(params[:id])
+    end
+
 	def new
       @job = Job.new
-	end
-
-	def show
-		@job = Job.find(params[:id])
 	end
 
 	def create
 	  @job = Job.new(job_params)
 	  if @job.save
-	  	redirect_to jobs_path
+	  	redirect_to admin_jobs_path
 	  else
 	  	render :new
 	  end
@@ -28,7 +29,7 @@ class JobsController < ApplicationController
 	def update
 		@job = job.find(params[:id])
 		if @job.update(job_params)
-			redirect_to jobs_path
+			redirect_to admin_jobs_path
 		else
 			render :edit
 		end
@@ -37,7 +38,7 @@ class JobsController < ApplicationController
 	def destroy
 		@job = Job.find(params[:id])
 		@job.destroy
-		redirect_to jobs_path
+		redirect_to admin_jobs_path
 		flash[:warning] = "是否删除?"
 	end
 
@@ -48,6 +49,4 @@ class JobsController < ApplicationController
 	def job_params
 		params.require(:job).permit(:title, :description)
 	end
-
-
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def required_admin
+  	if !current_user.admin?
+  		flash[:alert] = "你没有权限!"
+  		redirect_to root_path
+  	end
+  end
 end
